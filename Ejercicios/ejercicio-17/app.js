b7792d47d772df66abb0b9a0880baca9390c4c84
dsotogc <davidsotgar3@gmail.com>
2025-12-12 09:05:32 +0100

    ej 17

--- Ejercicios/ejercicio-17/app.js
+++ Ejercicios/ejercicio-17/app.js
@@ -7,7 +7,7 @@ si la introduces en la URL del navegador.
 document.addEventListener("DOMContentLoaded", obtenerDatos);
 
 function obtenerDatos() {
-  const NASA_API = "DEMO_KEY";
+  const NASA_API = "ObQae7PeC6WLZSfTOat2RbdqElVU1Gghvm0iapsu";
   const ruta = `https://api.nasa.gov/planetary/apod?api_key=${NASA_API}`;
 
   fetch(ruta)
@@ -15,6 +15,22 @@ function obtenerDatos() {
     .then((resultado) => mostrarDatos(resultado));
 }
 function mostrarDatos( resultado) {
-	// Tu código aquí
+	
+	console.log(resultado);
 
+	let titulo = document.querySelector('#titulo');
+	titulo.innerHTML = resultado.title;
+	let fecha = document.querySelector('#fecha');
+	fecha.innerHTML = resultado.date;
+	let descripcion = document.querySelector('#descripcion');
+	descripcion.innerHTML = resultado.explanation;
+	let multimedia = document.querySelector('#c_multimedia');
+	if (resultado.media_type == 'image')
+		multimedia.innerHTML = `<img src=${resultado.hdurl} width=1000>`;
+	else
+		multimedia.innerHTML = `<video src=${resultado.hdurl} width=1000></video>`
+	
 }
+
+obtenerDatos();
+
